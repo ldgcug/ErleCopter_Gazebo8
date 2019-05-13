fd4d2bd532ea94078932c7f007666eff7a70ed71
ldgcug <569167692@qq.com>
2019-05-13 19:29:07 +0800

    first commit

---
+++ aruco-1.3.0/build/utils_hrm/CMakeFiles/aruco_hrm_create_dictionary.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aruco_hrm_create_dictionary.dir/aruco_hrm_create_dictionary.cpp.o"
+  "aruco_hrm_create_dictionary.pdb"
+  "aruco_hrm_create_dictionary"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aruco_hrm_create_dictionary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ aruco-1.3.0/build/utils_hrm/CMakeFiles/aruco_hrm_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aruco_hrm_test.dir/aruco_hrm_test.cpp.o"
+  "aruco_hrm_test.pdb"
+  "aruco_hrm_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aruco_hrm_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
